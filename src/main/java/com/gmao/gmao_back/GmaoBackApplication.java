e9133901626a5acd2a24bd5b158b6746d660cd9e
joelnk <joelnkouatchet@gmail.com>
2022-07-10 18:22:25 -0700

    add infor

---
+++ src/main/java/com/gmao/gmao_back/GmaoBackApplication.java
@@ -0,0 +1,13 @@
+package com.gmao.gmao_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmaoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmaoBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmao/gmao_back/GmaoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmao.gmao_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmaoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
